a47024081791c690346e1df4ba0a22b70b67cc9e
Angelo Birrer <angelo.birrer@gmail.com>
2017-03-11 12:40:49 +0100

    Added Bias to the system

--- backprop_3.py
+++ backprop_3.py
@@ -7,22 +7,27 @@ def nonlin(x,deriv=False):
 	else:
 		return 1/(1+np.exp(-x))
 
+input_size = 3
+sample_length = 5
 
 X = np.array([
 	[0,0,1],
 	[0,1,1],
 	[1,1,0],
-	[0,0,0]
+	[0,0,0],
+	[1,0,0]
 	])
-y = np.array([[0,1,1,0]]).T
+y = np.array([[0,1,1,0,0]]).T
 
 np.random.seed(1)
 
-syn0 = 2*np.random.random((3,1)) -1 #Get values between 1 and -1
+		#Consider the bias as well
+syn0 = 2*np.random.random((input_size+1,1)) -1 #Get values between 1 and -1
 
 
 for j in range(0,10000):
-	l0 = X
+	bias = np.ones((sample_length,1))
+	l0 = np.concatenate((X,bias), axis=1)
 	l1 = nonlin(np.dot(l0,syn0))
 	l1_error = y -l1
 	delta_l1 = l1_error * nonlin(l1, True)
@@ -31,4 +36,4 @@ for j in range(0,10000):
 		print("Error: " + str(np.mean(np.abs(l1_error))))
 
 print("After Training:")
-print(nonlin(np.dot(X,syn0)))
+print(l1)
